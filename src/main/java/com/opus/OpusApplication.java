d913f61fe723060076f97a305746376aadd4d448
saurabhthecodewizard <skmahajan4998@gmail.com>
2023-10-10 01:56:11 +0530

    initial commit

---
+++ src/main/java/com/opus/OpusApplication.java
@@ -0,0 +1,13 @@
+package com.opus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opus/OpusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
