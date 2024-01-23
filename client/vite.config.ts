6966c99e756dcef2e7b974c88a97570f4d43b51b
saurabhthecodewizard <skmahajan4998@gmail.com>
2024-01-23 06:14:35 +0530

    docker-compose setup complete

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,5 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
+  },
+  // To allow the host to be exposed from container
+  preview: {
+    host: true,
+    port: 3000
   }
 })
